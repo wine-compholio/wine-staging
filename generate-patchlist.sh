6cc7c450bfefc2d36b9dc9793915978c205fc246
Sebastian Lackner <sebastian@fds-team.de>
2013-11-22 20:06:28 +0100

    generate-patchlist.sh: Don't include files without any author and title, regenerated patchlist

--- generate-patchlist.sh
+++ generate-patchlist.sh
@@ -5,6 +5,9 @@ for FILE in patches/*.patch; do
 	MD5SUM=$(md5sum "${FILE}" | sed 's| .*||g');
 	AUTHOR=$(cat "${FILE}" | sed -n 's|From: \([^<]*\).*|\1|p' | sed -e 's|"||g' -e 's| $||g');
 	TITLE=$(cat "${FILE}" | sed -n '1!N; s|Subject: \(.*\)\n|\1|p');
+	if [ "${AUTHOR}" = "" ] && [ "${TITLE}" = "" ]; then
+		continue;
+	fi
 	if [ "${PATCH_DATA}" != "" ]; then
 		PATCH_DATA="${PATCH_DATA}
 ";

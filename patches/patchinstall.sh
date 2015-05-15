26e06cc15129eb6910101cea156f89fc905f1862
Sebastian Lackner <sebastian@fds-team.de>
2015-05-15 21:38:48 +0200

    Added patches for additional tests of wininet cookie/header/authentication handling, in preparation of various cleanup patches.

--- patches/patchinstall.sh
+++ patches/patchinstall.sh
@@ -276,6 +276,7 @@ patch_enable_all ()
 	enable_winex11_Window_Style="$1"
 	enable_winex11_XEMBED="$1"
 	enable_winex11_wglShareLists="$1"
+	enable_wininet_Cleanup="$1"
 	enable_wininet_Memory_Leak="$1"
 	enable_wininet_ParseX509EncodedCertificateForListBoxEntry="$1"
 	enable_winmm_Delay_Import_Depends="$1"
@@ -895,6 +896,9 @@ patch_enable ()
 			;;
 		winex11-wglShareLists)
 			enable_winex11_wglShareLists="$2"
+			;;
+		wininet-Cleanup)
+			enable_wininet_Cleanup="$2"
 			;;
 		wininet-Memory_Leak)
 			enable_wininet_Memory_Leak="$2"
@@ -5493,6 +5497,22 @@ if test "$enable_winex11_wglShareLists" -eq 1; then
 	patch_apply winex11-wglShareLists/0001-winex11.drv-Only-warn-about-used-contexts-in-wglShar.patch
 	(
 		echo '+    { "Michael Müller", "winex11.drv: Only warn about used contexts in wglShareLists.", 1 },';
+	) >> "$patchlist"
+fi
+
+# Patchset wininet-Cleanup
+# |
+# | Modified files:
+# |   *	dlls/wininet/tests/http.c
+# |
+if test "$enable_wininet_Cleanup" -eq 1; then
+	patch_apply wininet-Cleanup/0001-wininet-tests-Add-more-tests-for-cookies.patch
+	patch_apply wininet-Cleanup/0002-wininet-tests-Add-tests-for-overriding-host-header.patch
+	patch_apply wininet-Cleanup/0003-wininet-tests-Test-auth-credential-reusage-with-host.patch
+	(
+		echo '+    { "Michael Müller", "wininet/tests: Add more tests for cookies.", 1 },';
+		echo '+    { "Michael Müller", "wininet/tests: Add tests for overriding host header.", 1 },';
+		echo '+    { "Michael Müller", "wininet/tests: Test auth credential reusage with host override.", 1 },';
 	) >> "$patchlist"
 fi
 

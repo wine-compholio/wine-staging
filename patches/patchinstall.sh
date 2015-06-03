0abc5f4b7bba4012863dcb8191d319f3816948df
Sebastian Lackner <sebastian@fds-team.de>
2015-06-03 05:44:56 +0200

    Added additional tests for behaviour of opening readonly files.

--- patches/patchinstall.sh
+++ patches/patchinstall.sh
@@ -2965,18 +2965,20 @@ fi
 # Patchset server-File_Permissions
 # |
 # | Modified files:
-# |   *	dlls/advapi32/tests/security.c, server/fd.c
+# |   *	dlls/advapi32/tests/security.c, dlls/ntdll/tests/file.c, server/fd.c
 # |
 if test "$enable_server_File_Permissions" -eq 1; then
 	patch_apply server-File_Permissions/0001-server-Allow-to-open-files-without-any-permission-bi.patch
 	patch_apply server-File_Permissions/0002-server-When-creating-new-directories-temporarily-giv.patch
 	patch_apply server-File_Permissions/0003-advapi32-tests-Add-tests-for-ACL-inheritance-in-Crea.patch
 	patch_apply server-File_Permissions/0004-advapi32-tests-Add-ACL-inheritance-tests-for-creatin.patch
+	patch_apply server-File_Permissions/0005-ntdll-tests-Added-tests-for-open-behaviour-on-readon.patch
 	(
 		echo '+    { "Sebastian Lackner", "server: Allow to open files without any permission bits.", 2 },';
 		echo '+    { "Sebastian Lackner", "server: When creating new directories temporarily give read-permissions until they are opened.", 1 },';
 		echo '+    { "Sebastian Lackner", "advapi32/tests: Add tests for ACL inheritance in CreateDirectoryA.", 1 },';
 		echo '+    { "Sebastian Lackner", "advapi32/tests: Add ACL inheritance tests for creating subdirectories with NtCreateFile.", 1 },';
+		echo '+    { "Qian Hong", "ntdll/tests: Added tests for open behaviour on readonly files.", 1 },';
 	) >> "$patchlist"
 fi
 

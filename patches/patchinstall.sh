8f245e2d607ed662c4ebdf0731c353939c9f926f
Sebastian Lackner <sebastian@fds-team.de>
2015-07-17 04:17:57 +0200

    mscoree-CorValidateImage: Replace check for __x86_64__ with _WIN64.

--- patches/patchinstall.sh
+++ patches/patchinstall.sh
@@ -3125,7 +3125,7 @@ fi
 if test "$enable_mscoree_CorValidateImage" -eq 1; then
 	patch_apply mscoree-CorValidateImage/0001-mscoree-Implement-_CorValidateImage.patch
 	(
-		echo '+    { "Michael Müller", "mscoree: Implement _CorValidateImage.", 1 },';
+		echo '+    { "Michael Müller", "mscoree: Implement _CorValidateImage.", 2 },';
 	) >> "$patchlist"
 fi
 

e9f445ed37b429b478c98159f4db9cae2123e71b
Sebastian Lackner <sebastian@fds-team.de>
2015-12-13 01:53:38 +0100

    server-Parent_Process: Add tests and some minor improvements.

--- patches/patchinstall.sh
+++ patches/patchinstall.sh
@@ -4998,13 +4998,18 @@ fi
 # |   *	[#37087] Do not hold reference on parent process in wineserver
 # |
 # | Modified files:
-# |   *	server/console.c, server/process.c, server/process.h, server/snapshot.c, server/thread.c, server/token.c
+# |   *	dlls/kernel32/tests/process.c, server/console.c, server/process.c, server/process.h, server/snapshot.c, server/thread.c,
+# | 	server/token.c
 # |
 if test "$enable_server_Parent_Process" -eq 1; then
-	patch_apply server-Parent_Process/0001-server-token_duplicate-should-not-reference-the-orig.patch
-	patch_apply server-Parent_Process/0002-server-Increase-size-of-PID-table-to-512-to-reduce-r.patch
-	patch_apply server-Parent_Process/0003-server-Do-not-hold-reference-on-parent-process.patch
+	patch_apply server-Parent_Process/0001-kernel32-tests-Remove-unnecessary-call-to-GetExitCod.patch
+	patch_apply server-Parent_Process/0002-kernel32-tests-Add-test-for-process-object-destructi.patch
+	patch_apply server-Parent_Process/0003-server-token_duplicate-should-not-reference-the-orig.patch
+	patch_apply server-Parent_Process/0004-server-Increase-size-of-PID-table-to-512-to-reduce-r.patch
+	patch_apply server-Parent_Process/0005-server-Do-not-hold-reference-on-parent-process.patch
 	(
+		echo '+    { "Sebastian Lackner", "kernel32/tests: Remove unnecessary call to GetExitCodeProcess in process tests.", 1 },';
+		echo '+    { "Sebastian Lackner", "kernel32/tests: Add test for process object destruction.", 1 },';
 		echo '+    { "Sebastian Lackner", "server: Token_duplicate should not reference the original token, which will get destroyed on process exit.", 1 },';
 		echo '+    { "Sebastian Lackner", "server: Increase size of PID table to 512 to reduce risk of collisions.", 1 },';
 		echo '+    { "Sebastian Lackner", "server: Do not hold reference on parent process.", 1 },';

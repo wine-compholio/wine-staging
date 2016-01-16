a293741c331b1e3b3166614badeb3b415d0b1db1
Sebastian Lackner <sebastian@fds-team.de>
2016-01-16 09:54:17 +0100

    Added patch for api-ms-win-core-apiquery-l1-1-0 dll.

--- patches/patchinstall.sh
+++ patches/patchinstall.sh
@@ -2491,10 +2491,11 @@ fi
 # |
 # | Modified files:
 # |   *	configure.ac, dlls/api-ms-win-appmodel-runtime-l1-1-1/Makefile.in, dlls/api-ms-win-appmodel-runtime-l1-1-1/api-ms-win-
-# | 	appmodel-runtime-l1-1-1.spec, dlls/api-ms-win-core-com-l1-1-1/Makefile.in, dlls/api-ms-win-core-com-l1-1-1/api-ms-win-
-# | 	core-com-l1-1-1.spec, dlls/api-ms-win-core-delayload-l1-1-1/Makefile.in, dlls/api-ms-win-core-delayload-l1-1-1/api-ms-
-# | 	win-core-delayload-l1-1-1.spec, dlls/api-ms-win-core-quirks-l1-1-0/Makefile.in, dlls/api-ms-win-core-quirks-l1-1-0/api-
-# | 	ms-win-core-quirks-l1-1-0.spec, dlls/api-ms-win-crt-heap-l1-1-0/api-ms-win-crt-heap-l1-1-0.spec,
+# | 	appmodel-runtime-l1-1-1.spec, dlls/api-ms-win-core-apiquery-l1-1-0/Makefile.in, dlls/api-ms-win-core-apiquery-l1-1-0
+# | 	/api-ms-win-core-apiquery-l1-1-0.spec, dlls/api-ms-win-core-com-l1-1-1/Makefile.in, dlls/api-ms-win-core-com-l1-1-1/api-
+# | 	ms-win-core-com-l1-1-1.spec, dlls/api-ms-win-core-delayload-l1-1-1/Makefile.in, dlls/api-ms-win-core-delayload-l1-1-1
+# | 	/api-ms-win-core-delayload-l1-1-1.spec, dlls/api-ms-win-core-quirks-l1-1-0/Makefile.in, dlls/api-ms-win-core-
+# | 	quirks-l1-1-0/api-ms-win-core-quirks-l1-1-0.spec, dlls/api-ms-win-crt-heap-l1-1-0/api-ms-win-crt-heap-l1-1-0.spec,
 # | 	dlls/kernelbase/Makefile.in, dlls/kernelbase/kernelbase.spec, dlls/kernelbase/misc.c, dlls/ucrtbase/ucrtbase.spec,
 # | 	tools/make_specfiles
 # |
@@ -2505,6 +2506,7 @@ if test "$enable_api_ms_win_crt_Stub_DLLs" -eq 1; then
 	patch_apply api-ms-win-crt-Stub_DLLs/0004-api-ms-win-core-quirks-l1-1-0-Add-dll.patch
 	patch_apply api-ms-win-crt-Stub_DLLs/0005-api-ms-win-core-delayload-l1-1-1-Add-dll.patch
 	patch_apply api-ms-win-crt-Stub_DLLs/0006-api-ms-win-appmodel-runtime-l1-1-1-Add-new-dll.patch
+	patch_apply api-ms-win-crt-Stub_DLLs/0007-api-ms-win-core-apiquery-l1-1-0-Add-dll.patch
 	(
 		echo '+    { "Martin Storsjo", "ucrtbase: Hook up some functions with new names to existing implementations.", 1 },';
 		echo '+    { "Michael Müller", "api-ms-win-core-com-l1-1-1: Add dll.", 1 },';
@@ -2512,6 +2514,7 @@ if test "$enable_api_ms_win_crt_Stub_DLLs" -eq 1; then
 		echo '+    { "Michael Müller", "api-ms-win-core-quirks-l1-1-0: Add dll.", 1 },';
 		echo '+    { "Michael Müller", "api-ms-win-core-delayload-l1-1-1: Add dll.", 1 },';
 		echo '+    { "Michael Müller", "api-ms-win-appmodel-runtime-l1-1-1: Add new dll.", 1 },';
+		echo '+    { "Michael Müller", "api-ms-win-core-apiquery-l1-1-0: Add dll.", 1 },';
 	) >> "$patchlist"
 fi
 

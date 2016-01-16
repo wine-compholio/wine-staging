9666dbdff7b83edc5ce6d63a9a109bc9e1f56be9
Sebastian Lackner <sebastian@fds-team.de>
2016-01-17 00:04:55 +0100

    Added patch for api-ms-win-core-kernel32-legacy-l1-1-1 dll.

--- patches/patchinstall.sh
+++ patches/patchinstall.sh
@@ -2498,11 +2498,12 @@ fi
 # | 	appmodel-runtime-l1-1-1.spec, dlls/api-ms-win-core-apiquery-l1-1-0/Makefile.in, dlls/api-ms-win-core-apiquery-l1-1-0
 # | 	/api-ms-win-core-apiquery-l1-1-0.spec, dlls/api-ms-win-core-com-l1-1-1/Makefile.in, dlls/api-ms-win-core-com-l1-1-1/api-
 # | 	ms-win-core-com-l1-1-1.spec, dlls/api-ms-win-core-delayload-l1-1-1/Makefile.in, dlls/api-ms-win-core-delayload-l1-1-1
-# | 	/api-ms-win-core-delayload-l1-1-1.spec, dlls/api-ms-win-core-libraryloader-l1-2-0/Makefile.in, dlls/api-ms-win-core-
-# | 	libraryloader-l1-2-0/api-ms-win-core-libraryloader-l1-2-0.spec, dlls/api-ms-win-core-quirks-l1-1-0/Makefile.in, dlls
-# | 	/api-ms-win-core-quirks-l1-1-0/api-ms-win-core-quirks-l1-1-0.spec, dlls/api-ms-win-crt-heap-l1-1-0/api-ms-win-crt-
-# | 	heap-l1-1-0.spec, dlls/kernelbase/Makefile.in, dlls/kernelbase/kernelbase.spec, dlls/kernelbase/misc.c,
-# | 	dlls/ucrtbase/ucrtbase.spec, tools/make_specfiles
+# | 	/api-ms-win-core-delayload-l1-1-1.spec, dlls/api-ms-win-core-kernel32-legacy-l1-1-1/Makefile.in, dlls/api-ms-win-core-
+# | 	kernel32-legacy-l1-1-1/api-ms-win-core-kernel32-legacy-l1-1-1.spec, dlls/api-ms-win-core-
+# | 	libraryloader-l1-2-0/Makefile.in, dlls/api-ms-win-core-libraryloader-l1-2-0/api-ms-win-core-libraryloader-l1-2-0.spec,
+# | 	dlls/api-ms-win-core-quirks-l1-1-0/Makefile.in, dlls/api-ms-win-core-quirks-l1-1-0/api-ms-win-core-quirks-l1-1-0.spec,
+# | 	dlls/api-ms-win-crt-heap-l1-1-0/api-ms-win-crt-heap-l1-1-0.spec, dlls/kernelbase/Makefile.in,
+# | 	dlls/kernelbase/kernelbase.spec, dlls/kernelbase/misc.c, dlls/ucrtbase/ucrtbase.spec, tools/make_specfiles
 # |
 if test "$enable_api_ms_win_Stub_DLLs" -eq 1; then
 	patch_apply api-ms-win-Stub_DLLs/0001-ucrtbase-Hook-up-some-functions-with-new-names-to-ex.patch
@@ -2513,6 +2514,7 @@ if test "$enable_api_ms_win_Stub_DLLs" -eq 1; then
 	patch_apply api-ms-win-Stub_DLLs/0006-api-ms-win-appmodel-runtime-l1-1-1-Add-new-dll.patch
 	patch_apply api-ms-win-Stub_DLLs/0007-api-ms-win-core-apiquery-l1-1-0-Add-dll.patch
 	patch_apply api-ms-win-Stub_DLLs/0008-api-ms-win-core-libraryloader-l1-2-0-Add-dll.patch
+	patch_apply api-ms-win-Stub_DLLs/0009-api-ms-win-core-kernel32-legacy-l1-1-1-Add-dll.patch
 	(
 		echo '+    { "Martin Storsjo", "ucrtbase: Hook up some functions with new names to existing implementations.", 1 },';
 		echo '+    { "Michael Müller", "api-ms-win-core-com-l1-1-1: Add dll.", 1 },';
@@ -2522,6 +2524,7 @@ if test "$enable_api_ms_win_Stub_DLLs" -eq 1; then
 		echo '+    { "Michael Müller", "api-ms-win-appmodel-runtime-l1-1-1: Add new dll.", 1 },';
 		echo '+    { "Michael Müller", "api-ms-win-core-apiquery-l1-1-0: Add dll.", 1 },';
 		echo '+    { "Michael Müller", "api-ms-win-core-libraryloader-l1-2-0: Add dll.", 1 },';
+		echo '+    { "Michael Müller", "api-ms-win-core-kernel32-legacy-l1-1-1: Add dll.", 1 },';
 	) >> "$patchlist"
 fi
 

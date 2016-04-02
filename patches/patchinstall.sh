a3ecf01ce291a620409d4af8acd1e2028f9a57ad
Sebastian Lackner <sebastian@fds-team.de>
2016-04-02 22:34:58 +0200

    Added patch to implement support for converting 16 bit depth to 24 bit in msvidc32.

--- patches/patchinstall.sh
+++ patches/patchinstall.sh
@@ -195,6 +195,7 @@ patch_enable_all ()
 	enable_msvcr120__SetWinRTOutOfMemoryExceptionCallback="$1"
 	enable_msvcrt_Math_Precision="$1"
 	enable_msvfw32_ICGetDisplayFormat="$1"
+	enable_msvidc32_Convert_Bitness="$1"
 	enable_ntdll_APC_Performance="$1"
 	enable_ntdll_APC_Start_Process="$1"
 	enable_ntdll_Activation_Context="$1"
@@ -763,6 +764,9 @@ patch_enable ()
 			;;
 		msvfw32-ICGetDisplayFormat)
 			enable_msvfw32_ICGetDisplayFormat="$2"
+			;;
+		msvidc32-Convert_Bitness)
+			enable_msvidc32_Convert_Bitness="$2"
 			;;
 		ntdll-APC_Performance)
 			enable_ntdll_APC_Performance="$2"
@@ -4595,6 +4599,21 @@ if test "$enable_msvfw32_ICGetDisplayFormat" -eq 1; then
 		echo '+    { "Michael Müller", "msvfw32: Try different formarts in ICGetDisplayFormat.", 1 },';
 		echo '+    { "Michael Müller", "msvfw32: Add test for negative width/height values passed to ICGetDisplayFormat.", 1 },';
 		echo '+    { "Michael Müller", "msvfw32: Set biSizeImage correctly in ICGetDisplayFormat.", 1 },';
+	) >> "$patchlist"
+fi
+
+# Patchset msvidc32-Convert_Bitness
+# |
+# | This patchset fixes the following Wine bugs:
+# |   *	[#14695] Implement support for converting 16 bit depth to 24 bit in msvidc32
+# |
+# | Modified files:
+# |   *	dlls/msvfw32/tests/msvfw.c, dlls/msvidc32/msvideo1.c
+# |
+if test "$enable_msvidc32_Convert_Bitness" -eq 1; then
+	patch_apply msvidc32-Convert_Bitness/0001-msvidc32-Add-support-for-converting-16-bit-depth-to-.patch
+	(
+		echo '+    { "Michael Müller", "msvidc32: Add support for converting 16 bit depth to 24 bit.", 1 },';
 	) >> "$patchlist"
 fi
 

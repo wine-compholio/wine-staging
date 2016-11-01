8682bdd67ac07d778589f4b27bb82a94099ef1c7
Sebastian Lackner <sebastian@fds-team.de>
2016-11-01 21:34:22 +0100

    Release 1.9.22.

--- patches/patchinstall.sh
+++ patches/patchinstall.sh
@@ -58,7 +58,7 @@ upstream_commit()
 # Show version information
 version()
 {
-	echo "Wine Staging 1.9.22 (unreleased)"
+	echo "Wine Staging 1.9.22"
 	echo "Copyright (C) 2014-2016 the Wine Staging project authors."
 	echo ""
 	echo "Patchset to be applied on upstream Wine:"

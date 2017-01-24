fa1d5938f24640b099e9b3e609137c0ae3b75bcc
Sebastian Lackner <sebastian@fds-team.de>
2017-01-24 05:04:18 +0100

    Added patch to recognize sha256/384/512 KEY OIDs.

--- patches/patchinstall.sh
+++ patches/patchinstall.sh
@@ -109,6 +109,7 @@ patch_enable_all ()
 	enable_crypt32_CMS_Certificates="$1"
 	enable_crypt32_Certificate_Check="$1"
 	enable_crypt32_CryptUnprotectMemory="$1"
+	enable_crypt32_SHA_OIDs="$1"
 	enable_d3d10_1_Forwards="$1"
 	enable_d3d11_Deferred_Context="$1"
 	enable_d3d11_ID3D11Texture1D="$1"
@@ -542,6 +543,9 @@ patch_enable ()
 			;;
 		crypt32-CryptUnprotectMemory)
 			enable_crypt32_CryptUnprotectMemory="$2"
+			;;
+		crypt32-SHA_OIDs)
+			enable_crypt32_SHA_OIDs="$2"
 			;;
 		d3d10_1-Forwards)
 			enable_d3d10_1_Forwards="$2"
@@ -3086,6 +3090,18 @@ if test "$enable_crypt32_CryptUnprotectMemory" -eq 1; then
 	patch_apply crypt32-CryptUnprotectMemory/0001-crypt32-Print-CryptUnprotectMemory-FIXME-only-once.patch
 	(
 		printf '%s\n' '+    { "Christian Costa", "crypt32: Print CryptUnprotectMemory FIXME only once.", 1 },';
+	) >> "$patchlist"
+fi
+
+# Patchset crypt32-SHA_OIDs
+# |
+# | Modified files:
+# |   *	dlls/crypt32/oid.c, include/wincrypt.h
+# |
+if test "$enable_crypt32_SHA_OIDs" -eq 1; then
+	patch_apply crypt32-SHA_OIDs/0001-crypt32-Recognize-sha256-384-512-KEY-OIDs.patch
+	(
+		printf '%s\n' '+    { "Michael Müller", "crypt32: Recognize sha256/384/512 KEY OIDs.", 1 },';
 	) >> "$patchlist"
 fi
 

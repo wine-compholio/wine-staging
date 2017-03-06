3b947eae95842c2c996d89947d1f0d085e03ce13
Sebastian Lackner <sebastian@fds-team.de>
2017-03-06 22:06:33 +0100

    ntoskrnl-InStackQueuedSpinLock: Use the correct macro.

--- patches/patchinstall.sh
+++ patches/patchinstall.sh
@@ -58,7 +58,7 @@ upstream_commit()
 # Show version information
 version()
 {
-	echo "Wine Staging 2.3"
+	echo "Wine Staging 2.4 (unreleased)"
 	echo "Copyright (C) 2014-2017 the Wine Staging project authors."
 	echo ""
 	echo "Patchset to be applied on upstream Wine:"

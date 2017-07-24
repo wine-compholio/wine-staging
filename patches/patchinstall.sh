f47fa538c58bf83ca976f51c1b9bcea5cdabcd30
Sebastian Lackner <sebastian@fds-team.de>
2017-07-24 18:58:35 +0200

    Added patch to fix context backup window handling.

--- patches/patchinstall.sh
+++ patches/patchinstall.sh
@@ -436,6 +436,7 @@ patch_enable_all ()
 	enable_winecfg_Unmounted_Devices="$1"
 	enable_wined3d_1DTextures="$1"
 	enable_wined3d_Accounting="$1"
+	enable_wined3d_Backup_Window="$1"
 	enable_wined3d_CSMT_Helper="$1"
 	enable_wined3d_CSMT_Main="$1"
 	enable_wined3d_Copy_Resource_Typeless="$1"
@@ -1571,6 +1572,9 @@ patch_enable ()
 			;;
 		wined3d-Accounting)
 			enable_wined3d_Accounting="$2"
+			;;
+		wined3d-Backup_Window)
+			enable_wined3d_Backup_Window="$2"
 			;;
 		wined3d-CSMT_Helper)
 			enable_wined3d_CSMT_Helper="$2"
@@ -9304,6 +9308,18 @@ if test "$enable_wined3d_Accounting" -eq 1; then
 	patch_apply wined3d-Accounting/0001-wined3d-Use-real-values-for-memory-accounting-on-NVI.patch
 	(
 		printf '%s\n' '+    { "Michael Müller", "wined3d: Use real values for memory accounting on NVIDIA cards.", 1 },';
+	) >> "$patchlist"
+fi
+
+# Patchset wined3d-Backup_Window
+# |
+# | Modified files:
+# |   *	dlls/wined3d/context.c
+# |
+if test "$enable_wined3d_Backup_Window" -eq 1; then
+	patch_apply wined3d-Backup_Window/0001-wined3d-Fix-context-backup-window-handling.patch
+	(
+		printf '%s\n' '+    { "Sebastian Lackner", "wined3d: Fix context backup window handling.", 1 },';
 	) >> "$patchlist"
 fi
 

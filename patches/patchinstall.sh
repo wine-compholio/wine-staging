22cdd720aabcc283f97e569f469d209bc1ad5e9c
Sebastian Lackner <sebastian@fds-team.de>
2017-09-20 02:14:38 +0200

    Release 2.17.

--- patches/patchinstall.sh
+++ patches/patchinstall.sh
@@ -58,7 +58,7 @@ upstream_commit()
 # Show version information
 version()
 {
-	echo "Wine Staging 2.17 (unreleased)"
+	echo "Wine Staging 2.17"
 	echo "Copyright (C) 2014-2017 the Wine Staging project authors."
 	echo ""
 	echo "Patchset to be applied on upstream Wine:"

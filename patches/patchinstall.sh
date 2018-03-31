e31b1ba24f87ddc4a2744ab864b4cf911a39d4dc
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2018-03-31 17:38:09 +1100

    Release v3.5

--- patches/patchinstall.sh
+++ patches/patchinstall.sh
@@ -58,7 +58,7 @@ upstream_commit()
 # Show version information
 version()
 {
-	echo "Wine Staging 3.5 (unreleased)"
+	echo "Wine Staging 3.5"
 	echo "Copyright (C) 2014-2018 the Wine Staging project authors."
 	echo "Copyright (C) 2018 Alistair Leslie-Hughes"
 	echo ""

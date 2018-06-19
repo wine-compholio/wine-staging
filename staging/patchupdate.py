3168a74745b5c945e80b0dfb129c92840d83f2dc
Zebediah Figura <z.figura12@gmail.com>
2018-06-19 16:10:43 -0500

    patchupdate.py: Allow linking to specific patches.

--- staging/patchupdate.py
+++ staging/patchupdate.py
@@ -431,9 +431,11 @@ def check_bug_status(all_patches, sync_bugs=False):
                                                   bug['resolution'], bug['cf_staged_patchset'])
             if sync_bugs:
                 sync_bug_status(bugtracker, bug, url_map[bug['id']])
+        patchset = bug['cf_staged_patchset']
+        if '.patch' in patchset: patchset = patchset[0:patchset.rindex('/')].replace('/blob/','/tree/')
         if bug['status'] == 'STAGED' and \
-           bug['cf_staged_patchset'] != url_map[bug['id']] and \
-           bug['cf_staged_patchset'] != url_map[bug['id']].replace('github.com/wine-staging','github.com/wine-compholio'):
+           patchset != url_map[bug['id']] and \
+           patchset != url_map[bug['id']].replace('github.com/wine-staging','github.com/wine-compholio'):
             print 'Invalid staged patchset: #%d - \"%s\" - %s' %(bug['id'], bug['summary'], bug['cf_staged_patchset'])
 
     once = True

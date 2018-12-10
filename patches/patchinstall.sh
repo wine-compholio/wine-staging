66148cb9b2760b4a311ef4a18ec7104b4eddd798
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2018-12-11 09:27:54 +1100

    Updated wintab32-improvements patchset

--- patches/patchinstall.sh
+++ patches/patchinstall.sh
@@ -7560,12 +7560,14 @@ if test "$enable_wintab32_improvements" -eq 1; then
 	patch_apply wintab32-improvements/0003-winex11-Handle-negative-orAltitude-values.patch
 	patch_apply wintab32-improvements/0004-winex11.drv-Support-multiplex-categories-WTI_DSCTXS-.patch
 	patch_apply wintab32-improvements/0005-winex11-Support-WTI_STATUS-in-WTInfo.patch
+	patch_apply wintab32-improvements/0006-winex11-Use-active-owner-when-sending-messages.patch
 	(
 		printf '%s\n' '+    { "Eriks Dobelis", "winex11: Implement PK_CHANGE for wintab.", 1 },';
 		printf '%s\n' '+    { "Alistair Leslie-Hughes", "wintab32: Set lcSysExtX/Y for the first index of WTI_DDCTXS.", 1 },';
 		printf '%s\n' '+    { "Alistair Leslie-Hughes", "winex11: Handle negative orAltitude values.", 1 },';
 		printf '%s\n' '+    { "Alistair Leslie-Hughes", "winex11.drv: Support multiplex categories WTI_DSCTXS and WTI_DDCTXS.", 1 },';
 		printf '%s\n' '+    { "Alistair Leslie-Hughes", "winex11: Support WTI_STATUS in WTInfo.", 1 },';
+		printf '%s\n' '+    { "Robert Walker", "winex11: Use active owner when sending messages.", 1 },';
 	) >> "$patchlist"
 fi
 

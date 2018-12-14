31d525e567bf41b8159cec66697fd526c1484522
Zebediah Figura <z.figura12@gmail.com>
2018-12-13 19:03:58 -0600

    newupdate.py: Also regenerate patchinstall.sh.

--- staging/newupdate.py
+++ staging/newupdate.py
@@ -88,6 +88,7 @@ def main():
     subprocess.call(['autoreconf','-f'],cwd=winedir)
     print 'Calling tools/make_requests...'
     subprocess.call(['./tools/make_requests'],cwd=winedir)
+    subprocess.call(['./staging/patchupdate.sh','--skip-checks','--skip-bugs'])
     sys.exit(0)
 
 if __name__ == '__main__':

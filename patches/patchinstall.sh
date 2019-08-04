9917fb4559d4b5f08ef65c85c4be6d211f636bd8
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2019-08-05 08:56:21 +1000

    Updated wintab32-improvements patchset
    Dropped scalling patch as it's causing regressions.

--- patches/patchinstall.sh
+++ patches/patchinstall.sh
@@ -7335,13 +7335,11 @@ if test "$enable_wintab32_improvements" -eq 1; then
 	patch_apply wintab32-improvements/0003-winex11-Handle-negative-orAltitude-values.patch
 	patch_apply wintab32-improvements/0004-winex11.drv-Support-multiplex-categories-WTI_DSCTXS-.patch
 	patch_apply wintab32-improvements/0005-winex11-Support-WTI_STATUS-in-WTInfo.patch
-	patch_apply wintab32-improvements/0006-wintab32-Scale-NormalPressure-before-sending-to-the-.patch
 	(
 		printf '%s\n' '+    { "Alistair Leslie-Hughes", "wintab32: Set lcSysExtX/Y for the first index of WTI_DDCTXS.", 1 },';
 		printf '%s\n' '+    { "Alistair Leslie-Hughes", "winex11: Handle negative orAltitude values.", 1 },';
 		printf '%s\n' '+    { "Alistair Leslie-Hughes", "winex11.drv: Support multiplex categories WTI_DSCTXS and WTI_DDCTXS.", 1 },';
 		printf '%s\n' '+    { "Alistair Leslie-Hughes", "winex11: Support WTI_STATUS in WTInfo.", 1 },';
-		printf '%s\n' '+    { "Alistair Leslie-Hughes", "wintab32: Scale NormalPressure before sending to the client.", 1 },';
 	) >> "$patchlist"
 fi
 

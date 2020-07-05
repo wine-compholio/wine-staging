4f13965432d5dfa7bf11a9f7cf84ebba8fbe5225
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2020-07-06 07:54:38 +1000

    Release v5.12.1
    Compile Fixes.

--- patches/patchinstall.sh
+++ patches/patchinstall.sh
@@ -58,7 +58,7 @@ upstream_commit()
 # Show version information
 version()
 {
-	echo "Wine Staging 5.12"
+	echo "Wine Staging 5.12.1"
 	echo "Copyright (C) 2014-2019 the Wine Staging project authors."
 	echo "Copyright (C) 2018-2020 Alistair Leslie-Hughes"
 	echo ""
